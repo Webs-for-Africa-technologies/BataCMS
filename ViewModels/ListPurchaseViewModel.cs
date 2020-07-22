88e0763de1bb171813187f5ee42a11fd4adfedda
GuduPaul <gudupaul18@gmail.com>
2020-07-22 14:45:09 +0200

    Added the logic to show Admin user purchases resolves#20

---
+++ ViewModels/ListPurchaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BataCMS.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BataCMS.ViewModels
+{
+    public class ListPurchaseViewModel
+    {
+        public IEnumerable<Purchase> Purchases { get; set; }
+
+    }
+}
